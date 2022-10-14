afbe07b7ab0aa793f09424921ddc2bb8d88e43a7
Sangwon <Sangwon@Sangwons-MacBookPro.local>
2022-10-14 15:07:19 +0800

    Reverted Flask

--- website/views.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, render_template, request, flash, jsonify
-from . import db
-import json
-
-views = Blueprint('views', __name__)
-
-@views.route('/', methods=['GET', 'POST'])
-def home():
-    return render_template("home.html")
